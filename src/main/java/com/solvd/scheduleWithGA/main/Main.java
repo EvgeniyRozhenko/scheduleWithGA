352336d544f7cffc53924acb7294d14940ccded3
Dzotz <danilo.zotov@gmail.com>
2021-09-27 21:55:13 +0300

    try to solve conflicts

--- src/main/java/com/solvd/scheduleWithGA/main/Main.java
+++ src/main/java/com/solvd/scheduleWithGA/main/Main.java
@@ -1,10 +1,8 @@
 package com.solvd.scheduleWithGA.main;
 
 import com.solvd.scheduleWithGA.binary.ClassGroup;
-import com.solvd.scheduleWithGA.binary.Schedule;
 import com.solvd.scheduleWithGA.binary.Teacher;
 import com.solvd.scheduleWithGA.dao.implementation.*;
-import org.apache.ibatis.type.ByteArrayTypeHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,14 +12,20 @@ public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ClassGroupService classGroupService = new ClassGroupService();
+        LOGGER.info(classGroupService.getClassGroupById(2));
+        LessonService lessonService = new LessonService();
+        LOGGER.info(lessonService.getLessonById(2));
+        TeacherService teacherService = new TeacherService();
+        LOGGER.info(teacherService.getTeacherById(1));
+        TimeSlotService timeSlotService = new TimeSlotService();
 
-        ScheduleService scheduleService = new ScheduleService();
-        LOGGER.info(scheduleService.getScheduleByDayId(1));
-        LessonsInGroupService lessonsInGroupService = new LessonsInGroupService();
-        LOGGER.info(lessonsInGroupService.getLessonInGroupId(1));
-        TimePeriodService timePeriodService = new TimePeriodService();
-        LOGGER.info(timePeriodService.getTimePeriodById(1));
-        WeekDayService weekDayService = new WeekDayService();
-        LOGGER.info(weekDayService.getWeekDayById(1));
+
+        TeacherService teacherService1 = new TeacherService();
+        HashMap<Integer, Teacher> x = teacherService.getTeachersHashMap();
+
+        LOGGER.info(timeSlotService.getTimslotById(1));
+        ClassroomService classroomService = new ClassroomService();
+        LOGGER.info(classroomService.getClassroomById(1));
     }
 }
